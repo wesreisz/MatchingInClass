1e4578b6047d1efa412c792ec91dc1b4348b7842
Wesley Reisz <wes@wesleyreisz.com>
2015-02-11 18:50:18 -0500

    added initial project structure

---
+++ src/MatchingInClass/Gruntfile.js
@@ -0,0 +1,29 @@
+﻿/*
+This file in the main entry point for defining grunt tasks and using grunt plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
+*/
+module.exports = function (grunt) {
+	grunt.initConfig({
+		bower: {
+			install: {
+				options: {
+					targetDir: "wwwroot/lib",
+					layout: "byComponent",
+					cleanTargetDir: false
+				}
+			}
+		},
+		// Add this JSON object:
+		less: {
+			development: {
+				options: {
+					paths: ["Assets"],
+				},
+				files: { "wwwroot/css/styles.css": "assets/*.less" }
+			},
+		}
+	});
+	grunt.registerTask("default", ["bower:install"]);
+	grunt.loadNpmTasks("grunt-bower-task");
+	grunt.loadNpmTasks("grunt-contrib-less");
+};
